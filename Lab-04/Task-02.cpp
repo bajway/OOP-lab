c7620458148033e485962dbb18441d807ffb3671
bajway <kashifbaj54321@gmail.com>
2025-02-14 13:26:51 +0500

    Update Task-02.cpp

--- Lab-04/Task-02.cpp
+++ Lab-04/Task-02.cpp
@@ -25,12 +25,34 @@ class WeekDays{
         this->days[6]="Sunday";
         CurrentDay =day % 7;
     }
-    string get_Day(){
-        
-    }
-
-
-};
-int main(){
-
+string getCurrentDay()  
+        {
+            return Days[CurrentDay] ;
+        }
+        string getNextDay() 
+        {
+            return Days[(CurrentDay + 1) % 7] ;
+        }
+        string getPreviousDay()  
+        {
+            return Days[(CurrentDay + 6) % 7] ;
+        }
+        string getNthDayFromToday(int N)
+        {
+            return Days[(CurrentDay + N) % 7] ;
+        }
+} ;
+int main() 
+{
+    WeekDays day1 ;
+    cout<<"Current Day:"<<day1.getCurrentDay()<<"\n" ;
+    cout<<"Next Day:"<<day1.getNextDay()<<"\n" ;
+    cout<<"Previous Day:"<<day1.getPreviousDay()<<"\n" ;
+    cout<<"20 Days From Today:"<< day1.getNthDayFromToday(20)<<"\n\n" ;
+    WeekDays day2(8) ;
+    cout<<"Current Day:"<<day2.getCurrentDay()<<"\n" ;
+    cout<<"Next Day:"<<day2.getNextDay()<< "\n" ;
+    cout<<"Previous Day:"<<day2.getPreviousDay()<<"\n" ;
+    cout<<"20 Days From Today:"<<day2.getNthDayFromToday(20)<<"\n" ;
+    return 0 ; 
 }
